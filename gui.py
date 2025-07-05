4722fdc9ee89b95c2c803336bc62797346a424ef
dibarra3 <160536459+dibarra3@users.noreply.github.com>
2025-07-05 11:18:06 -0500

    Created basic GUI with no interactions

---
+++ gui.py
@@ -0,0 +1,12 @@
+import functions
+import FreeSimpleGUI as sg
+
+label = sg.Text("Type in a to-do")
+input_box = sg.InputText(tooltip="Enter a todo")
+add_button = sg.Button("Add")
+
+window = sg.Window("My To-Do App", layout=[[label], [input_box, add_button]])
+#[] means one line on gui
+
+window.read()
+window.close()
