12271bb83eefb24f396dadfc80761eb48d2bcc4d
dibarra3 <160536459+dibarra3@users.noreply.github.com>
2025-07-07 19:55:21 -0500

    Implemented Complete and Exit feature

--- gui.py
+++ gui.py
@@ -9,8 +9,14 @@ edit_button = sg.Button("Edit")
 list_box = sg.Listbox(values=functions.get_todos(), key="todos",
                       enable_events=True, size=(45, 10))
 
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
+
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font= ["Helvetica", 20])
 #[] means one line on gui
 
@@ -34,8 +40,21 @@ while True:
             todos[index] = new_todo
             functions.write_todos(todos)
             window["todos"].update(values=todos)
+
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["todos"].update(values=todos)
+            # window["to-do"].update(value=f"TO-DO: {todo_to_complete} - removed... ")
+            window["todo"].update(value="")
+
         case "todos":
             window["todo"].update(value=values["todos"][0])
+
+        case "Exit":
+            break
         case sg.WIN_CLOSED:
             break
         # exit()
